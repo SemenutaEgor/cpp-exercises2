14d1c3085dcffaf10597cdaa6b371ad75bd1c625
egor <egor@Ubuntu2204.myguest.virtualbox.org>
2026-01-19 19:14:09 +0300

    build: added ability to off clang-tidy

--- calculator/cmake/ClangTidy.cmake
+++ calculator/cmake/ClangTidy.cmake
@@ -1,6 +1,8 @@
+option(USE_CLANG_TIDY "Enable clang-tidy checks" ON)
+
 find_program(CLANG_TIDY_EXE NAMES clang-tidy)
 
-if(CLANG_TIDY_EXE)
+if(CLANG_TIDY_EXE AND USE_CLANG_TIDY)
     message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
     function(enable_clang_tidy target)
         if(TARGET ${target})
@@ -10,7 +12,7 @@ if(CLANG_TIDY_EXE)
         endif()
     endfunction()
 else()
-    message(STATUS "clang-tidy not found")
+    message(STATUS "clang-tidy not used")
     function(enable_clang_tidy target)
     endfunction()
 endif()
