bfba1ea923f646bd520b4ef1293bf6f57651836a
egor <egor@Ubuntu2204.myguest.virtualbox.org>
2026-02-01 22:31:26 +0300

    refactor: introduce normalized OperationKey

--- calculator/src/core/Runner.cpp
+++ calculator/src/core/Runner.cpp
@@ -2,15 +2,38 @@
 
 #include <istream>
 #include <ostream>
+#include <sstream>
 
 #include "Errors.h"
 #include "logging/Logger.h"
+
+#include "../storage/OperationKey.h" // временно для проверки
 
 int Runner::run(std::istream& in, std::ostream& out,
                 std::ostream& err) noexcept {
   try {
     Logger::instance().info("Application started");
     auto request = parser_.parse(in);
+
+    // TODO для проверки хеширования
+
+    auto key = makeOperationKey(request);
+
+    std::ostringstream oss;
+    oss << "OperationKey: op=" << request.operation() << ", args_count=" << key.args.size();
+
+    if (!key.args.empty()) {
+     oss << ", first_arg=" << key.args[0];
+    }
+
+    if (key.args.size() > 1) {
+     oss << ", second_arg=" << key.args[1];
+    }
+
+    Logger::instance().info(oss.str());
+
+    //
+
     checker_.validate(request);
     auto result = calculator_.calculate(request);
     printer_.print(request, result, out);
